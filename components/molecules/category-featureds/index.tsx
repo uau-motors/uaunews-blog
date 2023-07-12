e52777deb26e5d5670fdf280c48f6fa51794324b
José Roberto <joseroberto.toko@gmail.com>
2023-07-12 09:02:21 -0300

    adjust category featured

--- components/molecules/category-featureds/index.tsx
+++ components/molecules/category-featureds/index.tsx
@@ -1,24 +1,13 @@
 import React from "react";
 
 import { Box, Grid } from "@mui/material";
-import PostImage from "@molecules/post-card-thumb";
-import PostTitle from "@components/atoms/post-title";
-import PostDate from "@molecules/post-date";
-import PostViews from "@molecules/post-views";
-
-import data from "@utility/data/posts.json";
 import PostCard from "@molecules/post-card";
-import PostThumb from "@molecules/post-thumb";
 import { PostCardDataI } from "@utility/interfaces";
 
 interface FeaturedPostsProps {
   posts: PostCardDataI[];
 }
 const CategoryFeatureds = ({ posts }: FeaturedPostsProps) => {
-  const handleClick = () => {
-    console.log("O botão foi clicado!");
-  };
-
   return (
     <Box className="featureds-category">
       <Box className="visible-xs">
